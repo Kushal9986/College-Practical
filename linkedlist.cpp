01f15b181cba095c5193b9684386ca1b6671af76
Kushal9986 <rajputkushal9986@gmail.com>
2023-07-24 18:41:25 +0530

    linkedlist updated

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -1,67 +1,100 @@
 #include<iostream>
-#include<stdlib.h>
 using namespace std;
 
-struct node
+struct node 
 {
 	int item;
-	struct node* next;
+	struct node*next;
 };
 
-void insertatlast(int data,struct node**s)
+void insertatlast(int data,struct node **s)
 {
-	    struct node *n;
-		struct node *t;
-	    n=(struct node*)malloc(sizeof(struct node));
-		n->item=data;
-		n->next=NULL;
-	
-	if (*s==NULL)
-   {
+	struct node*n;
+	n=(struct node*)malloc(sizeof(struct node));
+	n->item=data;
+	n->next=NULL;
+	if(*s==NULL)
+	{
 		*s=n;
 	}
 	
 	else
 	{
-			t=*s;
-				
-			while(t->next!=NULL)
-			t=t->next;
-			
-			t->next =n;
-	}
-}
-
-void deletefirst(struct node **s)
-{
-	struct node*t;
-	if(*s!=NULL)
-	{
+		struct node*t;
 		t=*s;
-		*s=t->next;
-		free(t);
+		while(t->next!=NULL)
+		{
+			t=t->next;
+		}
+		t->next=n;
 	}
 }
 
-void view(struct node*start)
-{
-	while(start)
+void view(struct node*s)
+{int i=1;
+	while(s)
 	{
-		cout<<start->item<<endl;
-		start=start->next;
+	    
+		cout<<"value at node "<<i<<" is "<<s->item<<endl;
+		s=s->next;
+		i=i+1;
 	}
 }
+
+void deletefirst(struct node**s)
+{
+	struct node *t;
+	t=*s;
+	*s=t->next;
+	free(t);
+}
+void insertatfirst(struct node** s,int data)
+{
+	struct node*n,*t;
+	n=(struct node*)malloc(sizeof(struct node));
+	n->item=data;
+	t=*s;
+	*s=n;
+	(*s)->next=t;
+}
+
+void deletelast(struct node*s)
+{
+	struct node*t,*t1;
+	t=s;
+	while(t->next!=NULL)
+	{	t1=t;
+		t=t->next;
+	}
+	t1->next =NULL;
+	free(t);
+}
+
 
 int main()
 {
-	struct node* start=NULL;
-	insertatlast(25,&start);
-	insertatlast(26,&start);
-	insertatlast(27,&start);
-	cout<<"\n";
+	
+	struct node *start=NULL;
+	insertatlast(20,&start);
+	insertatlast(30,&start);
+	insertatlast(40,&start);
 	view(start);
+	
 	deletefirst(&start);
-	cout<<"\n";
+	
+	cout<<"\t\t\t\t\tnodes after deleting first node"<<endl;
+	view(start);
+	
+	insertatfirst(&start,10);
+	cout<<"\t\t\t\t\tnodes after adding value at first node"<<endl;
+	view(start);
+	
+	deletefirst(&start);
+	cout<<"\t\t\t\t\tnodes after deleting first node"<<endl;
+	view(start);
+	
+	deletelast(start);
+	cout<<"\t\t\t\t\tnodes after deleting last node"<<endl;
 	view(start);
 	return 0;
 }
